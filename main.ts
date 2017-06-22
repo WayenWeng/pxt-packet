89d50d3012bf61a9e7f445cb31d498db1898dcd9
WayenWeng <jinyuan.weng@seeed.cc>
2017-06-22 09:46:46 +0800

    Update

--- main.ts
+++ main.ts
@@ -1,75 +1,33 @@
 
 /**
- * 
- */
-enum SINGLE_PIXEL_TYPE {
-    //% block=*
-    ON = 1,
-    //% block=.
-    OFF = 0
-}
-
-enum MATRIX_PIXEL_TYPE {
-    //% block=*
-    ON = 1,
-    //% block=.
-    OFF = 0
-}
-
-/**
  * Functions to operate packet test.
  */
 //% weight=5 color=#2699BF icon="\uf110"
-namespace packet {
+namespace packet {    
     /**
-     * Get input pin value 
-     * @param pin out pin
+     * Test item 1
      */
-    //% blockId=get_pin block="getPin pin %pin"
-    export function getPin(pin: DigitalPin):number {
-        let value = pins.digitalReadPin(pin);
-        return value;
-    }
-    
-    /**
-     * Set input pin value 
-     * @param pin out pin
-     * @param value out value
-     */
-    //% blockId=set_pin block="setPin pin %pin|value %value"
-    export function setPin(pin: DigitalPin, value: number) {
-        pins.digitalWritePin(pin, value);
-    }
-    
-    /**
-     * Test matrix 
-     * @param para1 
-     * @param para2 
-     */
-    //% blockId=test_matrix block="test matrix|%para1|%para2|%para3|%para4"
-    //% blockExternalInputs=true
-    //% imageLiteral=1
-    export function testMatrix(para1: number, para2: number, para3: number, para4: number) 
+    //% blockId=test_item_1 block="test item 1"
+    export function testItem1() 
     {
-        para1 = para2;
-        para3 = para4;
+
     }
     
     /**
-     * Test string 
-     * @param para
+     * Test item 2
      */
-    //% blockId=test_string block="test strig %para"
-    export function testString(para: string) {
-        
+    //% blockId=test_item_2 block="test item 2"
+    export function testItem2() 
+    {
+
     }
     
     /**
-     * Test buffer 
-     * @param para
+     * Test item 3
      */
-    //% blockId=test_buffer block="test buffer %para"
-    export function testBuffer(para: number[] = [0x00,0x00,0x00,0x00,0x00]) {
-        
+    //% blockId=test_item_3 block="test item 3"
+    export function testItem3() 
+    {
+
     }
 }
